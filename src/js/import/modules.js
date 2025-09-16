8e770a062d9a021d370339d1f60f8394311df468
FrontMD <web@mockup.digital>
2025-09-16 13:01:01 +0300

    buyback in progress

--- src/js/import/modules.js
+++ src/js/import/modules.js
@@ -2,10 +2,12 @@
 @@include("../../blocks/modules/modals/modals.js")
 @@include("../../blocks/modules/personal-data/personal-data.js")
 @@include("../../blocks/modules/auto-card/auto-card.js")
+@@include("../../blocks/modules/e-cars/e-cars.js")
 
 document.addEventListener('DOMContentLoaded', () => {
     modalsInit()
     headerController()
     pdContactsController()
     autoCardInit()
+    eCarsController()
 })
